3a1c176880721b101c564c01fe5e60c9022aec5e
Vancha <tjipkevanderheide90@gmail.com>
2025-06-26 00:47:46 +0200

    tag selection concept
    Send message on nav item select, this seems to make sense for selecting a tag, assuming we can get the correct tag based off of the ID

---
+++ app.rs
@@ -0,0 +1,384 @@
+// SPDX-License-Identifier: MPL-2.0
+
+use crate::components::document_store::DocumentStore;
+use crate::components::search_engine::DocumentSearchEngine;
+use crate::config::Config;
+use crate::fl;
+use crate::models::document::Document;
+use cosmic::app::context_drawer;
+use cosmic::cosmic_config::{self, CosmicConfigEntry};
+use cosmic::iced::{Alignment, Length, Padding, Pixels, Subscription};
+use cosmic::prelude::*;
+use cosmic::widget::{self, Column, Row, button, icon, menu, nav_bar, scrollable, text, text_input};
+use cosmic::{cosmic_theme, theme};
+use std::collections::HashMap;
+use std::path::PathBuf;
+
+const REPOSITORY: &str = env!("CARGO_PKG_REPOSITORY");
+const APP_ICON: &[u8] = include_bytes!("../resources/icons/hicolor/scalable/apps/icon.svg");
+
+/// The application model stores app-specific state used to describe its interface and
+/// drive its logic.
+pub struct AppModel {
+    search_field_buffer: String,
+    /// Application state which is managed by the COSMIC runtime.
+    core: cosmic::Core,
+    /// Display a context drawer with the designated page if defined.
+    context_page: ContextPage,
+    /// Contains items assigned to the nav bar panel.
+    nav: nav_bar::Model,
+    /// Key bindings for the application's menu bar.
+    key_binds: HashMap<menu::KeyBind, MenuAction>,
+    // Configuration data that persists between application runs.
+    config: Config,
+    //The object responsible for filtering and retrieving the documents
+    documents: Vec<Document>, //DocumentManager,
+
+    engine: DocumentSearchEngine,
+    store: DocumentStore,
+}
+
+/// Messages emitted by the application and its widgets.
+#[derive(Debug, Clone)]
+pub enum Message {
+    OpenRepositoryUrl,
+    ToggleContextPage(ContextPage),
+    DocumentsLoaded(Vec<Document>),
+    SearchFieldInputChanged(String),
+    UpdateConfig(Config),
+    LaunchUrl(String),
+    ChooseFile,
+    TagSelected(cosmic::widget::segmented_button::Entity),
+    AddFile(Option<PathBuf>),
+}
+
+/// Create a COSMIC application from the app model
+impl cosmic::Application for AppModel {
+    /// The async executor that will be used to run your application's commands.
+    type Executor = cosmic::executor::Default;
+
+    /// Data that your application receives to its init method.
+    type Flags = ();
+
+    /// Messages which the application and its widgets will emit.
+    type Message = Message;
+
+    /// Unique identifier in RDNN (reverse domain name notation) format.
+    const APP_ID: &'static str = "com.github.vancha.skriuwit";
+
+    fn core(&self) -> &cosmic::Core {
+        &self.core
+    }
+
+    fn core_mut(&mut self) -> &mut cosmic::Core {
+        &mut self.core
+    }
+
+    /// Initializes the application with any given flags and startup commands.
+    fn init(
+        core: cosmic::Core,
+        _flags: Self::Flags,
+    ) -> (Self, Task<cosmic::Action<Self::Message>>) {
+        // Create a nav bar with three page items.
+        let mut nav = nav_bar::Model::default();
+        nav.insert().text("finance").divider_above(true);
+        nav.insert().text("personal"); //.data::<Page>(Page::Page1);
+        nav.insert().text("work"); //.data::<Page>(Page::Page1);
+        nav.insert().text("insurance"); //.data::<Page>(Page::Page1);
+
+        let engine = DocumentSearchEngine::new();
+        let fut = engine.get_all_documents(); // can this be done prettier? probbably but i have borrow/moving compile errors
+        let store = DocumentStore::new();
+        // Construct the app model with the runtime's core.
+        let mut app = AppModel {
+            core,
+            context_page: ContextPage::default(),
+            nav,
+            search_field_buffer: String::from(""),
+            key_binds: HashMap::new(),
+            // Optional configuration file for an application.
+            config: cosmic_config::Config::new(Self::APP_ID, Config::VERSION)
+                .map(|context| match Config::get_entry(&context) {
+                    Ok(config) => config,
+                    Err((_errors, config)) => {
+                        // for why in errors {
+                        //     tracing::error!(%why, "error loading app config");
+                        // }
+
+                        config
+                    }
+                })
+                .unwrap_or_default(),
+            documents: vec![],
+            engine,
+            store,
+        };
+
+        // Create a startup task that sets the window title.
+        // Also make sure we start loading our documents from disk on app creation
+
+        let update_title = app.update_title();
+        let load_documents = cosmic::task::future(async {
+            let docs = fut; // can this be done prettier? probbably but i have borrow/moving compile errors 
+            // also do we need this?
+            // probably have to call DB at this stage and load saved settings
+            Message::DocumentsLoaded(docs)
+        });
+        (app, Task::batch(vec![update_title, load_documents]))
+    }
+
+    /// Elements to pack at the start of the header bar.
+    fn header_start(&self) -> Vec<Element<Self::Message>> {
+        let menu_bar = menu::bar(vec![menu::Tree::with_children(
+            //menu::root(fl!("view").into_fragment()), //Does not compile
+            Element::from(menu::root(fl!("view"))),
+            menu::items(
+                &self.key_binds,
+                vec![menu::Item::Button(fl!("about"), None, MenuAction::About)],
+            ),
+        )]);
+
+        vec![menu_bar.into()]
+    }
+
+    fn header_center(&self) -> Vec<Element<Self::Message>> {
+        vec![text::body("Personal Document Manager").into()]
+    }
+
+    /// Enables the COSMIC application to create a nav bar with this model.
+    fn nav_model(&self) -> Option<&nav_bar::Model> {
+        Some(&self.nav)
+    }
+
+    /// Display a context drawer if the context page is requested.
+    fn context_drawer(&self) -> Option<context_drawer::ContextDrawer<Self::Message>> {
+        if !self.core.window.show_context {
+            return None;
+        }
+
+        Some(match self.context_page {
+            ContextPage::About => context_drawer::context_drawer(
+                self.about(),
+                Message::ToggleContextPage(ContextPage::About),
+            )
+            .title(fl!("about")),
+        })
+    }
+
+    /// Describes the interface based on the current state of the application model.
+    ///
+    /// Application events will be processed through the view. Any messages emitted by
+    /// events received by widgets will be passed to the update method.
+    fn view(&self) -> Element<Self::Message> {
+        Column::from_vec(vec![
+            text_input("Search", &self.search_field_buffer)
+                .on_input(Message::SearchFieldInputChanged)
+                .into(),
+            button::text("Add Document")
+                .on_press(Message::ChooseFile)
+                .into(),
+            scrollable(
+                Column::from_vec(
+                    self.documents
+                        .iter()
+                        .map(|document| {
+                            Row::with_children(vec![
+                                icon(document.icon.clone())
+                                    .width(Length::Fixed(100.0))
+                                    .height(Length::Fixed(100.0))
+                                    .into(),
+                                Column::from_vec(vec![
+                                    text::heading(&document.title).into(),
+                                    button::text("BAGUETTE").into(),
+                                    text::body("Added: ".to_owned() + &document.added_date).into(),
+                                ])
+                                .into(),
+                            ])
+                            .width(Length::Fill)
+                            .into()
+                        })
+                        .collect::<Vec<_>>(),
+                )
+                .spacing(Pixels::from(20.0))
+                .width(Length::Fill),
+            )
+            .into(),
+        ])
+        .spacing(Pixels::from(20.0))
+        .padding(Padding::from(20))
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .into()
+    }
+
+    /// Register subscriptions for this application.
+    ///
+    /// Subscriptions are long-running async tasks running in the background which
+    /// emit messages to the application through a channel. They are started at the
+    /// beginning of the application, and persist through its lifetime.
+    fn subscription(&self) -> Subscription<Self::Message> {
+        Subscription::batch(vec![
+            // Watch for application configuration changes.
+            self.core()
+                .watch_config::<Config>(Self::APP_ID)
+                .map(|update| {
+                    // for why in update.errors {
+                    //     tracing::error!(?why, "app config error");
+                    // }
+
+                    Message::UpdateConfig(update.config)
+                }),
+        ])
+    }
+
+    /// Handles messages emitted by the application and its widgets.
+    ///
+    /// Tasks may be returned for asynchronous execution of code in the background
+    /// on the application's async runtime.
+    fn update(&mut self, message: Self::Message) -> Task<cosmic::Action<Self::Message>> {
+        match message {
+            Message::OpenRepositoryUrl => {
+                _ = open::that_detached(REPOSITORY);
+            }
+
+            Message::TagSelected(tag_id) => {
+                println!("Tag selected: {:?}",tag_id);
+            }
+            Message::SearchFieldInputChanged(content) => {
+                self.search_field_buffer = content;
+            }
+
+            Message::ToggleContextPage(context_page) => {
+                if self.context_page == context_page {
+                    // Close the context drawer if the toggled context page is the same.
+                    self.core.window.show_context = !self.core.window.show_context;
+                } else {
+                    // Open the context drawer to display the requested context page.
+                    self.context_page = context_page;
+                    self.core.window.show_context = true;
+                }
+            }
+
+            Message::DocumentsLoaded(documents) => {
+                self.documents = documents;
+                println!("loaded all documents, {} in total", self.documents.len());
+            }
+
+            Message::UpdateConfig(config) => {
+                println!("The config of this app has been changed: {:?}", config);
+                self.config = config;
+            }
+
+            Message::LaunchUrl(url) => match open::that_detached(&url) {
+                Ok(()) => {}
+                Err(err) => {
+                    eprintln!("failed to open {url:?}: {err}");
+                }
+            },
+
+            Message::ChooseFile => {
+                return cosmic::task::future(async {
+                    Message::AddFile(rfd::FileDialog::new().pick_file())
+                });
+            }
+
+            Message::AddFile(path) => {
+                if let Some(path) = path {
+                    let title = path
+                        .file_name()
+                        .and_then(|name| name.to_str())
+                        .unwrap_or("Unknown")
+                        .to_string();
+                    let date = "testdate".to_string();
+                    let doc = Document::from_fields(title, date, path);
+                    self.engine.add_document(doc.clone());
+                    self.documents.push(doc);
+                }
+            }
+        }
+        Task::none()
+    }
+
+    /// Called when a nav item is selected.
+    fn on_nav_select(&mut self, id: nav_bar::Id) -> Task<cosmic::Action<Self::Message>> {
+        // Activate the page in the model.
+        self.nav.activate(id);
+        //Can this return a message containing the ID of the tag, like Message::TagSelected(tag_id)?
+        //Task::none()
+         Task::done(cosmic::Action::App(Message::TagSelected(id)))
+    }
+}
+
+impl AppModel {
+    /// The about page for this app.
+    pub fn about(&self) -> Element<Message> {
+        let cosmic_theme::Spacing { space_xxs, .. } = theme::active().cosmic().spacing;
+
+        let icon = widget::svg(widget::svg::Handle::from_memory(APP_ICON));
+
+        let title = widget::text::title3(fl!("app-title"));
+
+        let hash = env!("VERGEN_GIT_SHA");
+        let short_hash: String = hash.chars().take(7).collect();
+        let date = env!("VERGEN_GIT_COMMIT_DATE");
+
+        let link = widget::button::link(REPOSITORY)
+            .on_press(Message::OpenRepositoryUrl)
+            .padding(0);
+
+        widget::column()
+            .push(icon)
+            .push(title)
+            .push(link)
+            .push(
+                widget::button::link(fl!(
+                    "git-description",
+                    hash = short_hash.as_str(),
+                    date = date
+                ))
+                .on_press(Message::LaunchUrl(format!("{REPOSITORY}/commits/{hash}")))
+                .padding(0),
+            )
+            .align_x(Alignment::Center)
+            .spacing(space_xxs)
+            .into()
+    }
+
+    /// Updates the header and window titles.
+    pub fn update_title(&mut self) -> Task<cosmic::Action<Message>> {
+        let mut window_title = fl!("app-title");
+
+        if let Some(page) = self.nav.text(self.nav.active()) {
+            window_title.push_str(" — ");
+            window_title.push_str(page);
+        }
+
+        if let Some(id) = self.core.main_window_id() {
+            self.set_window_title(window_title, id)
+        } else {
+            Task::none()
+        }
+    }
+}
+
+/// The context page to display in the context drawer.
+#[derive(Copy, Clone, Debug, Default, Eq, PartialEq)]
+pub enum ContextPage {
+    #[default]
+    About,
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum MenuAction {
+    About,
+}
+
+impl menu::action::MenuAction for MenuAction {
+    type Message = Message;
+
+    fn message(&self) -> Self::Message {
+        match self {
+            MenuAction::About => Message::ToggleContextPage(ContextPage::About),
+        }
+    }
+}
